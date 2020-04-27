52a1a0952ffaae98ee62a7e3800473a455adb519
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-04-27 21:36:04 +0900

    feat: Commit `Footer` standalone component

---
+++ src/components/standalones/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+const Footer: React.SFC<> = () => {
+  return (
+    <footer className="Footer">
+      <small>Copyright (c) 2012 - 2014 <strong>Tomohiro IKEDA</strong> &gt;&gt; <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Website</a></small>
+      <small><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer">音楽素材/魔王魂</a></small>
+    </footer>
+  );
+};
+
+export default connect()(Footer);
