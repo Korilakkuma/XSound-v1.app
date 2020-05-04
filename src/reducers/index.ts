55a821da144126b3b70ac486c74597e601ba30e7
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-05-04 18:00:00 +0900

    feat: Commit Reducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,37 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { ActionTypes } from '../actions/ActionTypes';
+
+function currentSoundSource(state = 'oscillator', action): string {
+  switch (action.type) {
+    case ActionTypes.CHANGE_CURRENT_SOUND_SOURCE:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+function analyserState(state = false, action): boolean {
+  switch (action.type) {
+    case ActionTypes.CHANGE_ANALYSER_STATE:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+function mmlState(state = false, action): boolean {
+  switch (action.type) {
+    case ActionTypes.CHANGE_MML_STATE:
+      return action.payload;
+    default:
+      return state;
+  }
+}
 
 export const rootReducer = (history: History) => combineReducers({
-  router : connectRouter(history)
+  currentSoundSource,
+  analyserState,
+  mmlState,
+  router: connectRouter(history)
 });
