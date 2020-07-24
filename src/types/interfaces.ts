6f259ba4d5c98e995f5788571ab0425d22194e0b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-07-24 23:32:00 +0900

    feat: Add interfaces for Action

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,6 +1,23 @@
+import { Action } from 'redux';
+
 export interface RIRInfo {
   url: string;
   value: number;
   label: string;
   group: string;
+}
+
+export interface CurrentSourceSourceAction extends Action {
+  type: 'CHANGE_CURRENT_SOUND_SOURCE';
+  payload: SoundSource;
+}
+
+export interface AnalyserStateAction extends Action {
+  type: 'CHANGE_ANALYSER_STATE';
+  payload: boolean;
+}
+
+export interface MMLStateAction extends Action {
+  type: 'CHANGE_MML_STATE';
+  payload: boolean;
 }
