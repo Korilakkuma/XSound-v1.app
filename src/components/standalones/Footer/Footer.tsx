0bb2b3f609e2d7be9acecfdc509247fb04a6efd3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-09-26 20:50:07 +0900

    fix: Fix copyright

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Footer: React.SFC<> = () => {
   return (
     <footer className="Footer">
-      <small>Copyright (c) 2012 - 2014 <strong>Tomohiro IKEDA</strong> &gt;&gt; <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Website</a></small>
+      <small>Copyright (c) 2012 <strong>Tomohiro IKEDA</strong> &gt;&gt; <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Website</a></small>
       <small><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer">音楽素材/魔王魂</a></small>
     </footer>
   );
