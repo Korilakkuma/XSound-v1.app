3d41aa54f61fb420866c96d2478af1d198935b46
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-11 23:12:00 +0900

    feat: Add story file for `Button` component

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Props, Button } from './Button';
+import '../../../main.css';
+
+export default {
+  title    : 'atoms/Button',
+  component: Button
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  active : false,
+  label  : 'Start',
+  width  : 70,
+  height : 33,
+  image  : 'https://xsound.app/assets/images/button-audio.png',
+  size   : '70px 99px',
+  onClick: (event: React.SyntheticEvent) => {
+    alert(`${event.type} start`);
+  }
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  active : true,
+  label  : 'Stop',
+  width  : 70,
+  height : 33,
+  image  : 'https://xsound.app/assets/images/button-audio.png',
+  size   : '70px 99px',
+  onClick: (event: React.SyntheticEvent) => {
+    alert(`${event.type} stop`);
+  }
+};
