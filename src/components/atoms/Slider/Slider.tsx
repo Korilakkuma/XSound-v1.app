0f5085512eef13ef66d7e1969e8ccccce610df4e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-19 10:33:35 +0900

    refactor: Delete `id` props and component is surrounded by `label`

--- src/components/atoms/Slider/Slider.tsx
+++ src/components/atoms/Slider/Slider.tsx
@@ -5,20 +5,21 @@ export interface Props {
   min: number;
   max: number;
   step: number;
-  id?: string;
   onChange(event: React.SyntheticEvent): void;
 }
 
 export const Slider: React.FC<Props> = (props: Props) => {
-  const { value, min, max, step, id, onChange } = props;
+  const { value, min, max, step, onChange } = props;
 
   const rate  = (value - min) / (max - min);
   const width = rate * 100;
 
   return (
     <div className="Slider">
-      <input type="range" id={id} value={value} min={min} max={max} step={step} onChange={onChange} />
-      <div className="Slider__value" style={{ width: `calc(${width}% - (${rate} * 28px))` }} />
+      <label>
+        <input type="range" value={value} min={min} max={max} step={step} onChange={onChange} />
+        <div className="Slider__value" style={{ width: `calc(${width}% - (${rate} * 28px))` }} />
+      </label>
     </div>
   );
 };
