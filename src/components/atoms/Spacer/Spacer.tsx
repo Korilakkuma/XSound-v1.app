d73f30f515665065b4f27bc50c38267259399638
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-20 19:38:51 +0900

    refactor: Fix export
    - Use named export instead of default export
    - Export interface for story file

--- src/components/atoms/Spacer/Spacer.tsx
+++ src/components/atoms/Spacer/Spacer.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-interface Props {
+export interface Props {
   space: 2 | 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32;
   direction?: 'Top' | 'Right' | 'Bottom' | 'Left';
 }
 
-const Spacer: React.FC<Props> = (props: Props) => {
+export const Spacer: React.FC<Props> = (props: Props) => {
   const { space, direction } = props;
   const margin = `margin${direction ? direction : 'Bottom'}`;
 
   return <div role="presentation" style={{ [margin]: `${space}px` }} />;
 };
-
-export default Spacer;
