c4179fea656cd5e7abf966799dbed5f9685c2304
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-21 02:28:55 +0900

    feat: Add story file for `Spinner` component

---
+++ src/components/atoms/Spinner/Spinner.stories.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useCallback } from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Props, Spinner } from './Spinner';
+import '../../../main.css';
+
+export default {
+  title    : 'atoms/Spinner',
+  component: Spinner
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => {
+  const [value, setValue] = useState<number>(0);
+
+  const onChange = useCallback((event: React.SyntheticEvent) => {
+    setValue((event.currentTarget as HTMLInputElement).valueAsNumber);
+  }, []);
+
+  const props = Object.assign({}, args, { value, onChange });
+
+  return (
+    <React.Fragment>
+      <label htmlFor={props.id} style={{ marginRight: '12px' }}>Spinner</label>
+      <Spinner {...props} />
+    </React.Fragment>
+  );
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  value: 0,
+  min  : -100,
+  max  : 100,
+  step : 1
+};
