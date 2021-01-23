b33e9caa97e9d62bf080741b896fbf00fb9da27f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-23 20:00:00 +0900

    feat: Add story file for `OscillatorSelector` component

---
+++ src/components/helpers/OscillatorSelector/OscillatorSelector.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Props, OscillatorSelector } from './OscillatorSelector';
+import '../../../main.css';
+
+export default {
+  title    : 'helpers/OscillatorSelector',
+  component: OscillatorSelector
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => <OscillatorSelector {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  radioName  : 'oscillator-selector',
+  defaultType: 'sawtooth',
+  onChange   : (event: React.SyntheticEvent) => {
+    alert(`${event.type} ${(event.currentTarget as HTMLInputElement).value}`);
+  }
+};
