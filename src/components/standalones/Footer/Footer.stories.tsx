d49b987da6f9e836fe352f81dc9edece8e2ae8c2
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-28 02:14:50 +0900

    feat: Add story file for `Footer` component

---
+++ src/components/standalones/Footer/Footer.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Footer } from './Footer';
+import '../../../main.css';
+
+export default {
+  title    : 'standalones/Footer',
+  component: Footer
+} as Meta;
+
+const Template: Story<void> = () => <Footer />;
+
+export const Primary = Template.bind({});
