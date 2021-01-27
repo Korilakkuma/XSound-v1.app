2b6a88bf463c72ca0edd6aecb9d7a54383afdc22
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-27 17:28:24 +0900

    feat: Add story file for `AudioFieldset` component

---
+++ src/components/standalones/AudioFieldset/AudioFieldset.stories.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Props, AudioFieldset } from './AudioFieldset';
+import '../../../main.css';
+
+import { X } from 'xsound';
+
+export default {
+  title    : 'standalones/AudioFieldset',
+  component: AudioFieldset
+} as Meta;
+
+const Template: Story<Props> = (args: Props) => {
+  useEffect(() => {
+    if (X('audio').module('pitchshifter').state()) {
+      return;
+    }
+
+    X('audio').module('pitchshifter').state(true);
+  });
+
+  return <AudioFieldset {...args} />;
+};
+
+export const Primary = Template.bind({});
