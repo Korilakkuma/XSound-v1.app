65bc0c209904f35352d174de9702662aa0b21f14
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-01-30 20:00:07 +0900

    feat: Add story file for `RecorderFieldset` component

---
+++ src/components/standalones/RecorderFieldset/RecorderFieldset.stories.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Props, RecorderFieldset } from './RecorderFieldset';
+import '../../../main.css';
+
+import { X } from 'xsound';
+
+export default {
+  title    : 'standalones/RecorderFieldset',
+  component: RecorderFieldset
+} as Meta;
+
+const Template: Story<Props> = () => {
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (loaded) {
+      return;
+    }
+
+    const constraints = { audio : true, video : false };
+
+    try {
+      X('stream').setup(constraints).ready();
+      X('stream').param('output', false);
+      X('stream').start();
+    } catch (error) {
+      window.alert(error.message);
+    }
+
+    setLoaded(true);
+  }, [loaded]);
+
+  return <RecorderFieldset sources={['stream']} />;
+};
+
+export const Primary = Template.bind({});
