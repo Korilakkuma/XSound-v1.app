183a3b1fadf22e43b940326c0409ad78a0a13cf8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-01 01:52:08 +0900

    refactor: Use React Hooks instead of class component

--- src/components/standalones/AutopanFieldset/AutopanFieldset.tsx
+++ src/components/standalones/AutopanFieldset/AutopanFieldset.tsx
@@ -1,80 +1,70 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Spacer } from '../../atoms/Spacer';
 import { Switch } from '../../atoms/Switch';
 import { ValueController } from '../../helpers/ValueController';
 import { X } from 'xsound';
 
-interface Props {
+export interface Props {
   sources: string[];
 }
 
-export default class AutopanFieldset extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-
-    this.onChangeState = this.onChangeState.bind(this);
-    this.onChangeDepth = this.onChangeDepth.bind(this);
-    this.onChangeRate  = this.onChangeRate.bind(this);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="AutopanFieldset">
-        <fieldset>
-          <legend>
-            <Switch
-              id="autopan-state"
-              label="Autopan"
-              defaultChecked={false}
-              onChange={this.onChangeState}
-            />
-          </legend>
-          <ValueController
-            label="Depth"
-            id="autopan-depth"
-            defaultValue={0}
-            min={0}
-            max={1}
-            step={0.05}
-            onChange={this.onChangeDepth}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Rate"
-            id="autopan-rate"
-            defaultValue={0}
-            min={0}
-            max={1}
-            step={0.05}
-            onChange={this.onChangeRate}
-          />
-          <Spacer space={8} />
-        </fieldset>
-      </div>
-    );
-  }
-
-  private onChangeState(event: React.SyntheticEvent): void {
+export const AutopanFieldset: React.FC<Props> = (props: Props) => {
+  const onChangeStateCallback = useCallback((event: React.SyntheticEvent) => {
     const state = (event.currentTarget as HTMLInputElement).checked;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('autopanner').state(state);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeDepth(event: React.SyntheticEvent): void {
+  const onChangeDepthCallback = useCallback((event: React.SyntheticEvent) => {
     const depth = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('autopanner').param('depth', depth);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeRate(event: React.SyntheticEvent): void {
+  const onChangeRateCallback = useCallback((event: React.SyntheticEvent) => {
     const rate = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('autopanner').param('rate', rate);
     });
-  }
-}
+  }, [props.sources]);
+
+  return (
+    <div className="AutopanFieldset">
+      <fieldset>
+        <legend>
+          <Switch
+            id="autopan-state"
+            label="Autopan"
+            defaultChecked={false}
+            onChange={onChangeStateCallback}
+          />
+        </legend>
+        <ValueController
+          label="Depth"
+          id="autopan-depth"
+          defaultValue={0}
+          min={0}
+          max={1}
+          step={0.05}
+          onChange={onChangeDepthCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Rate"
+          id="autopan-rate"
+          defaultValue={0}
+          min={0}
+          max={1}
+          step={0.05}
+          onChange={onChangeRateCallback}
+        />
+        <Spacer space={8} />
+      </fieldset>
+    </div>
+  );
+};
