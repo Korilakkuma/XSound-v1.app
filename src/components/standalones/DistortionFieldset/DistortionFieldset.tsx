2f91ea5f626d14a8b4b3632c3dc9ce82653651d3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-03 00:52:50 +0900

    refactor: Use React Hooks instead of class component

--- src/components/standalones/DistortionFieldset/DistortionFieldset.tsx
+++ src/components/standalones/DistortionFieldset/DistortionFieldset.tsx
@@ -1,196 +1,182 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Spacer } from '../../atoms/Spacer';
 import { Select } from '../../atoms/Select';
 import { Switch } from '../../atoms/Switch';
 import { ValueController } from '../../helpers/ValueController';
 import { X } from 'xsound';
 
-interface Props {
+export interface Props {
   sources: string[];
 }
 
-export default class DistortionFieldset extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-
-    this.onChangeState   = this.onChangeState.bind(this);
-    this.onChangeCurve   = this.onChangeCurve.bind(this);
-    this.onChangeSamples = this.onChangeSamples.bind(this);
-    this.onChangeAmount  = this.onChangeAmount.bind(this);
-    this.onChangeDrive   = this.onChangeDrive.bind(this);
-    this.onChangeColor   = this.onChangeColor.bind(this);
-    this.onChangeTone    = this.onChangeTone.bind(this);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="DistortionFieldset">
-        <fieldset>
-          <legend>
-            <Switch
-              id="distortion-state"
-              label="Distortion"
-              defaultChecked={false}
-              onChange={this.onChangeState}
-            />
-          </legend>
-          <Select
-            id="select-distortion"
-            label="Select Distortion"
-            values={[
-              'clean',
-              'crunch',
-              'overdrive',
-              'distortion',
-              'fuzz'
-            ]}
-            texts={[
-              'CLEAN',
-              'CRUNCH',
-              'OVERDRIVE',
-              'DISTORTION',
-              'FUZZ'
-            ]}
-            onChange={this.onChangeCurve}
-          />
-          <Spacer space={8} />
-          <Select
-            id="select-curve-size"
-            label="Select Curve Size"
-            values={[
-              '8',
-              '16',
-              '32',
-              '64',
-              '128',
-              '256',
-              '512',
-              '1024',
-              '2048',
-              '4096',
-              '8192',
-              '16384',
-              '32768',
-              '65536'
-            ]}
-            texts={[
-              '8',
-              '16',
-              '32',
-              '64',
-              '128',
-              '256',
-              '512',
-              '1024',
-              '2048',
-              '4096',
-              '8192',
-              '16384',
-              '32768',
-              '65536'
-            ]}
-            onChange={this.onChangeSamples}
-            defaultValue="32768"
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Amount"
-            id="distortion-amount"
-            defaultValue={0.5}
-            min={0.05}
-            max={0.95}
-            step={0.05}
-            onChange={this.onChangeAmount}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Drive"
-            id="distortion-drive"
-            defaultValue={1}
-            min={0}
-            max={100}
-            step={1}
-            onChange={this.onChangeDrive}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Color"
-            id="distortion-color"
-            defaultValue={2000}
-            min={20}
-            max={2000}
-            step={1}
-            onChange={this.onChangeColor}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Tone"
-            id="distortion-tone"
-            defaultValue={4000}
-            min={20}
-            max={8000}
-            step={1}
-            onChange={this.onChangeTone}
-          />
-          <Spacer space={8} />
-        </fieldset>
-      </div>
-    );
-  }
-
-  private onChangeState(event: React.SyntheticEvent): void {
+export const DistortionFieldset: React.FC<Props> = (props: Props) => {
+  const onChangeStateCallback = useCallback((event: React.SyntheticEvent) => {
     const state = (event.currentTarget as HTMLInputElement).checked;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').state(state);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeCurve(event: React.SyntheticEvent): void {
+  const onChangeCurveCallback = useCallback((event: React.SyntheticEvent) => {
     const curve = (event.currentTarget as HTMLInputElement).value;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').param('curve', curve);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeSamples(event: React.SyntheticEvent): void {
+  const onChangeSamplesCallback = useCallback((event: React.SyntheticEvent) => {
     const samples = parseInt((event.currentTarget as HTMLInputElement).value, 10);
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').param('samples', samples);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeAmount(event: React.SyntheticEvent): void {
+  const onChangeAmountCallback = useCallback((event: React.SyntheticEvent) => {
     const amount = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').param('amount', amount);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeDrive(event: React.SyntheticEvent): void {
+  const onChangeDriveCallback = useCallback((event: React.SyntheticEvent) => {
     const drive = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').param('drive', drive);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeColor(event: React.SyntheticEvent): void {
+  const onChangeColorCallback = useCallback((event: React.SyntheticEvent) => {
     const color = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').param('color', color);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeTone(event: React.SyntheticEvent): void {
+  const onChangeToneCallback = useCallback((event: React.SyntheticEvent) => {
     const tone = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('distortion').param('tone', tone);
     });
-  }
-}
+  }, [props.sources]);
+
+  return (
+    <div className="DistortionFieldset">
+      <fieldset>
+        <legend>
+          <Switch
+            id="distortion-state"
+            label="Distortion"
+            defaultChecked={false}
+            onChange={onChangeStateCallback}
+          />
+        </legend>
+        <Select
+          id="select-distortion"
+          label="Select Distortion"
+          values={[
+            'clean',
+            'crunch',
+            'overdrive',
+            'distortion',
+            'fuzz'
+          ]}
+          texts={[
+            'CLEAN',
+            'CRUNCH',
+            'OVERDRIVE',
+            'DISTORTION',
+            'FUZZ'
+          ]}
+          onChange={onChangeCurveCallback}
+        />
+        <Spacer space={8} />
+        <Select
+          id="select-curve-size"
+          label="Select Curve Size"
+          values={[
+            '8',
+            '16',
+            '32',
+            '64',
+            '128',
+            '256',
+            '512',
+            '1024',
+            '2048',
+            '4096',
+            '8192',
+            '16384',
+            '32768',
+            '65536'
+          ]}
+          texts={[
+            '8',
+            '16',
+            '32',
+            '64',
+            '128',
+            '256',
+            '512',
+            '1024',
+            '2048',
+            '4096',
+            '8192',
+            '16384',
+            '32768',
+            '65536'
+          ]}
+          onChange={onChangeSamplesCallback}
+          defaultValue="32768"
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Amount"
+          id="distortion-amount"
+          defaultValue={0.5}
+          min={0.05}
+          max={0.95}
+          step={0.05}
+          onChange={onChangeAmountCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Drive"
+          id="distortion-drive"
+          defaultValue={1}
+          min={0}
+          max={100}
+          step={1}
+          onChange={onChangeDriveCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Color"
+          id="distortion-color"
+          defaultValue={2000}
+          min={20}
+          max={2000}
+          step={1}
+          onChange={onChangeColorCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Tone"
+          id="distortion-tone"
+          defaultValue={4000}
+          min={20}
+          max={8000}
+          step={1}
+          onChange={onChangeToneCallback}
+        />
+        <Spacer space={8} />
+      </fieldset>
+    </div>
+  );
+};
