b257db0621ff74cca6754239cf579396bf2afda5
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-05 00:52:08 +0900

    refactor: Use React Hooks instead of class component

--- src/components/standalones/WahFieldset/WahFieldset.tsx
+++ src/components/standalones/WahFieldset/WahFieldset.tsx
@@ -1,118 +1,106 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Spacer } from '../../atoms/Spacer';
 import { Switch } from '../../atoms/Switch';
 import { ValueController } from '../../helpers/ValueController';
 import { X } from 'xsound';
 
-interface Props {
+export interface Props {
   sources: string[];
 }
 
-export default class WahFieldset extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-
-    this.onChangeState     = this.onChangeState.bind(this);
-    this.onChangeCutoff    = this.onChangeCutoff.bind(this);
-    this.onChangeDepth     = this.onChangeDepth.bind(this);
-    this.onChangeRate      = this.onChangeRate.bind(this);
-    this.onChangeResonance = this.onChangeResonance.bind(this);
-  }
-
-  render(): React.ReactNode {
-    return (
-      <div className="WahFieldset">
-        <fieldset>
-          <legend>
-            <Switch
-              id="wah-state"
-              label="Wah"
-              defaultChecked={false}
-              onChange={this.onChangeState}
-            />
-          </legend>
-          <ValueController
-            label="Cutoff"
-            id="wah-cutoff"
-            defaultValue={350}
-            min={350}
-            max={8000}
-            step={1}
-            onChange={this.onChangeCutoff}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Depth"
-            id="wah-depth"
-            defaultValue={0}
-            min={0}
-            max={0.9}
-            step={0.01}
-            onChange={this.onChangeDepth}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Rate"
-            id="wah-rate"
-            defaultValue={0}
-            min={0}
-            max={10}
-            step={0.05}
-            onChange={this.onChangeRate}
-          />
-          <Spacer space={8} />
-          <ValueController
-            label="Resonance"
-            id="wah-resonance"
-            defaultValue={1}
-            min={1}
-            max={20}
-            step={1}
-            onChange={this.onChangeResonance}
-          />
-          <Spacer space={8} />
-        </fieldset>
-      </div>
-    );
-  }
-
-  private onChangeState(event: React.SyntheticEvent): void {
+export const WahFieldset: React.FC<Props> = (props: Props) => {
+  const onChangeStateCallback = useCallback((event: React.SyntheticEvent) => {
     const state = (event.currentTarget as HTMLInputElement).checked;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('wah').state(state);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeCutoff(event: React.SyntheticEvent): void {
+  const onChangeCutoffCallback = useCallback((event: React.SyntheticEvent) => {
     const cutoff = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('wah').param('cutoff', cutoff);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeDepth(event: React.SyntheticEvent): void {
+  const onChangeDepthCallback = useCallback((event: React.SyntheticEvent) => {
     const depth = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('wah').param('depth', depth);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeRate(event: React.SyntheticEvent): void {
+  const onChangeRateCallback = useCallback((event: React.SyntheticEvent) => {
     const rate = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('wah').param('rate', rate);
     });
-  }
+  }, [props.sources]);
 
-  private onChangeResonance(event: React.SyntheticEvent): void {
+  const onChangeResonanceCallback = useCallback((event: React.SyntheticEvent) => {
     const resonance = (event.currentTarget as HTMLInputElement).valueAsNumber;
 
-    this.props.sources.forEach((source: string) => {
+    props.sources.forEach((source: string) => {
       X(source).module('wah').param('resonance', resonance);
     });
-  }
-}
+  }, [props.sources]);
+
+  return (
+    <div className="WahFieldset">
+      <fieldset>
+        <legend>
+          <Switch
+            id="wah-state"
+            label="Wah"
+            defaultChecked={false}
+            onChange={onChangeStateCallback}
+          />
+        </legend>
+        <ValueController
+          label="Cutoff"
+          id="wah-cutoff"
+          defaultValue={350}
+          min={350}
+          max={8000}
+          step={1}
+          onChange={onChangeCutoffCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Depth"
+          id="wah-depth"
+          defaultValue={0}
+          min={0}
+          max={0.9}
+          step={0.01}
+          onChange={onChangeDepthCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Rate"
+          id="wah-rate"
+          defaultValue={0}
+          min={0}
+          max={10}
+          step={0.05}
+          onChange={onChangeRateCallback}
+        />
+        <Spacer space={8} />
+        <ValueController
+          label="Resonance"
+          id="wah-resonance"
+          defaultValue={1}
+          min={1}
+          max={20}
+          step={1}
+          onChange={onChangeResonanceCallback}
+        />
+        <Spacer space={8} />
+      </fieldset>
+    </div>
+  );
+};
--- src/components/standalones/WahFieldset/index.ts
+++ src/components/standalones/WahFieldset/index.ts
@@ -1,3 +1,3 @@
-import WahFieldset from './WahFieldset';
+import { WahFieldset } from './WahFieldset';
 
 export { WahFieldset };
