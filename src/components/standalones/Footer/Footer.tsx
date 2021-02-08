17cc1c9b5118772d8e23978651bc348f3ec3309e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-08 17:27:00 +0900

    fix: Define props interface

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export const Footer: React.FC = () => {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Props {
+}
+
+export const Footer: React.FC<Props> = () => {
   return (
     <footer className="Footer">
       <small>Copyright (c) 2012 <strong>Tomohiro IKEDA</strong> &gt;&gt; <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Website</a></small>
