2f8f95b1e3ee24fc722ef217af96d942f2d94dfe
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-08 17:27:00 +0900

    fix: Define props interface

--- src/components/standalones/Footer/Footer.stories.tsx
+++ src/components/standalones/Footer/Footer.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { Footer } from './Footer';
+import { Props, Footer } from './Footer';
 import '../../../main.css';
 
 export default {
@@ -10,6 +10,6 @@ export default {
   component: Footer
 } as Meta;
 
-const Template: Story<void> = () => <Footer />;
+const Template: Story<Props> = () => <Footer />;
 
 export const Primary = Template.bind({});
