0506d54cd9affb1c2927f46b7b67d035b6cc4181
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-18 23:58:00 +0900

    test: Add test for `Button` component

---
+++ src/components/atoms/Button/Button.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Props, Button } from './Button';
+
+describe('atoms/Button', () => {
+  it('inactive', () => {
+    const props = {
+      active : false,
+      label  : 'Start',
+      width  : 70,
+      height : 33,
+      image  : 'https://xsound.app/assets/images/button-audio.png',
+      size   : '70px 99px',
+      onClick: (event: React.SyntheticEvent) => {
+        alert(event.type);
+      }
+    } as Props;
+
+    const tree = renderer.create(<Button {...props} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('active', () => {
+    const props = {
+      active : true,
+      label  : 'Stop',
+      width  : 70,
+      height : 33,
+      image  : 'https://xsound.app/assets/images/button-audio.png',
+      size   : '70px 99px',
+      onClick: (event: React.SyntheticEvent) => {
+        alert(event.type);
+      }
+    } as Props;
+
+    const tree = renderer.create(<Button {...props} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
