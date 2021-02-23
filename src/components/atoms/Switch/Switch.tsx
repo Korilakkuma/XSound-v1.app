9dbee12e49cebb4d10157a05dc009dd0e0c36af9
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-24 00:00:51 +0900

    fix: Display keyboard focus

--- src/components/atoms/Switch/Switch.tsx
+++ src/components/atoms/Switch/Switch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 export interface Props {
   id: string;
@@ -10,10 +10,24 @@ export interface Props {
 export const Switch: React.FC<Props> = (props: Props) => {
   const { id, label, checked, onChange } = props;
 
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  const onKeyDownCallback = useCallback((event: React.SyntheticEvent) => {
+    const nativeEvent = event.nativeEvent as KeyboardEvent;
+
+    if ((nativeEvent.code === 'Space') || (nativeEvent.keyCode === 13)) {
+      event.preventDefault();
+
+      if (checkboxRef.current) {
+        checkboxRef.current.click();
+      }
+    }
+  }, []);
+
   return (
-    <div className={`Switch${checked ? ' -checked' : ''}`}>
-      <label htmlFor={id}>{label}<span aria-label={`${label} switch`} /></label>
-      <input type="checkbox" id={id} checked={checked} className="visually-hidden" onChange={onChange} />
+    <div className={`Switch${checked ? ' -checked' : ''}`} onKeyDown={onKeyDownCallback}>
+      <input type="checkbox" ref={checkboxRef} id={id} checked={checked} className="visually-hidden" onChange={onChange} />
+      <label htmlFor={id} tabIndex={0}>{label}<span aria-label={`${label} switch`} /></label>
     </div>
   );
 };
