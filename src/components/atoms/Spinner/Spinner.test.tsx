4fdfaf379da4ced2a79aa86a6cb02352a9a50dd4
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-23 19:25:19 +0900

    test: Add test for `Spinner` component

---
+++ src/components/atoms/Spinner/Spinner.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Props, Spinner } from './Spinner';
+
+describe('atoms/Spinner', () => {
+  it('render', () => {
+    const props = {
+      value   : 0,
+      min     : -100,
+      max     : 100,
+      step    : 1,
+      id      : 'spinner',
+      onChange: (event: React.SyntheticEvent) => {
+        alert(event.type);
+      }
+    } as Props;
+
+    const tree = renderer.create(<Spinner {...props} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
