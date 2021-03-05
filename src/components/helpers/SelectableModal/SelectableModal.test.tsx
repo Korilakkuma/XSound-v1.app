e1e85fdb33301c6488f5e80aaa79895f46144d52
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-03-05 21:58:37 +0900

    test: Add test for `SelectableModal` component

---
+++ src/components/helpers/SelectableModal/SelectableModal.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Props, SelectableModal } from './SelectableModal';
+
+describe('helpers/SelectableModal', () => {
+  it('render', () => {
+    const props = {
+      isShow    : true,
+      hasOverlay: true,
+      title     : 'with overlay',
+      first     : {
+        label : 'first',
+        action: (event: React.SyntheticEvent) => {
+          alert(`${event.type} first`);
+        }
+      },
+      second    : {
+        label : 'second',
+        action: (event: React.SyntheticEvent) => {
+          alert(`${event.type} second`);
+        }
+      },
+      onClose   : (event: React.SyntheticEvent) => {
+        alert(event.type);
+      }
+    } as Props;
+
+    const tree = renderer.create(<SelectableModal {...props}><div>Contents</div></SelectableModal>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
