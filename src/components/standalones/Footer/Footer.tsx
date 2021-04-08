69ffa14d5ad781d34c9b1286fb7283765aaf06f1
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-08 23:32:09 +0900

    fix: Fix lang attribute to `en`

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer: React.FC<Props> = () => {
   return (
     <footer className="Footer">
       <small>Copyright (c) 2012 <strong>Tomohiro IKEDA</strong> &gt;&gt; <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Website</a></small>
-      <small><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer">音楽素材/魔王魂</a></small>
+      <small><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></small>
     </footer>
   );
 };
