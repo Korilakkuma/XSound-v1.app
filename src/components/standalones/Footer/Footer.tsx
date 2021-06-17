12dcb7163d9307a64e8f89db1324adf8194f40f3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-06-17 23:54:40 +0900

    fix: Delete `small` tag

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -7,8 +7,8 @@ export interface Props {
 export const Footer: React.FC<Props> = () => {
   return (
     <footer className="Footer">
-      <small>Copyright (c) 2012 <strong>Tomohiro IKEDA</strong> &gt;&gt; <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Website</a></small>
-      <small><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></small>
+      <p>Copyright (c) 2012 <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Tomohiro IKEDA</a></p>
+      <p><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></p>
     </footer>
   );
 };
