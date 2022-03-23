4ae634d9cdfbb69e63594fae56b5ec984967e542
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-03-23 12:30:45 +0900

    chore: Storybook corresponds to webpack 5.x

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     {
@@ -14,5 +14,8 @@ module.exports = {
         }
       }
     }
-  ]
-}
+  ],
+  core: {
+    builder: "webpack5"
+  }
+};
