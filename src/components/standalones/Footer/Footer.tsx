e43b8ea117b892f671af859a1378eae6800f72a4
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-03-28 10:54:49 +0900

    fix: Fix link to author profile

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export interface Props {
 export const Footer: React.FC<Props> = () => {
   return (
     <footer className="Footer">
-      <p>Copyright (c) 2012 <a href="https://korilakkuma.github.io/LIFE-with-FAVORITES/" target="_blank" rel="noopener noreferrer">Tomohiro IKEDA</a></p>
+      <p>Copyright (c) 2012 <a href="https://www.youtube.com/channel/UCtW_oDhbAu7jCZiuQ58dmGw" target="_blank" rel="noopener noreferrer">Tomohiro IKEDA</a></p>
       <p><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></p>
     </footer>
   );
