6a575b7183ed3f9b765f8eee5451e9126870be0b
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-04-12 20:07:52 +0900

    refactor: Use `createRoot` instead of `ReactDOM`

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 'use strict';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { App } from './components/App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-);
+const container = document.getElementById('app');
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
