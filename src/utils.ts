6726f1dd7b5038d18aa14e8bdf7828b83eb71afe
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-06-29 23:59:13 +0900

    refactor: Use `padStart` instead of `slice`

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,24 @@
 import { ConvertedTime } from 'xsound';
 
+function padZero(n: number) {
+  return n.toString().padStart(2, '0');
+}
+
 export function formatAudioTime(convertedTime: ConvertedTime): string {
   const { minutes, seconds } = convertedTime;
 
-  return `${('0' + minutes).slice(-2)} : ${('0' + seconds).slice(-2)}`;
+  return `${padZero(minutes)} : ${padZero(seconds)}`;
 }
 
 export function createFilename(prefix: string, ext: string): string {
-  const format = (n: number) => {
-    return (`0${n}`).slice(-2);
-  };
-
   const date = new Date();
 
   const y = date.getFullYear();
-  const m = format(date.getMonth() + 1);
-  const d = format(date.getDate());
-  const h = format(date.getHours());
-  const i = format(date.getMinutes());
-  const s = format(date.getSeconds());
+  const m = padZero(date.getMonth() + 1);
+  const d = padZero(date.getDate());
+  const h = padZero(date.getHours());
+  const i = padZero(date.getMinutes());
+  const s = padZero(date.getSeconds());
 
   return `${prefix}${y}${m}${d}${h}${i}${s}.${ext}`;
 }
