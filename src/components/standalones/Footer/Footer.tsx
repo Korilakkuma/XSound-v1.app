aec415ac80b5cb0d55c478a9fdc9ee6ee156a6da
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-07-27 22:31:13 +0900

    fix: Fix footer link

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export interface Props {
 export const Footer: React.FC<Props> = () => {
   return (
     <footer className="Footer">
-      <p>Copyright (c) 2012 <a href="https://www.youtube.com/channel/UCtW_oDhbAu7jCZiuQ58dmGw" target="_blank" rel="noopener noreferrer">Tomohiro IKEDA</a></p>
+      <p>Copyright (c) 2012 <a href="https://github.com/Korilakkuma" target="_blank" rel="noopener noreferrer">Tomohiro IKEDA</a></p>
       <p><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></p>
     </footer>
   );
