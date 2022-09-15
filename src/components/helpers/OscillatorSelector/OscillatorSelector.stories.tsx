5637f3f758303a847ca3cf10a03ce56421b25bbd
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2022-09-15 17:28:45 +0900

    chore: Fix handler on changing oscillator type

--- src/components/helpers/OscillatorSelector/OscillatorSelector.stories.tsx
+++ src/components/helpers/OscillatorSelector/OscillatorSelector.stories.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 import { OscillatorSelector } from './OscillatorSelector';
 import '../../../main.css';
@@ -9,16 +9,36 @@ export default {
 } as ComponentMeta<typeof OscillatorSelector>;
 
 const Template: ComponentStoryObj<typeof OscillatorSelector> = {
-  render: (args) => <OscillatorSelector {...args} />
+  render: (args) => {
+    const [type, setType] = useState<OscillatorType>('sawtooth');
+
+    const onChange = useCallback((event: React.ChangeEvent<HTMLFormElement>) => {
+      const radios = event.currentTarget['radio-oscillator-selector'];
+
+      if (!radios) {
+        return;
+      }
+
+      // `document.forms['oscillator-selector'].elements['radio-oscillator-selector']`
+      for (const radio of radios) {
+        if (radio.checked) {
+          const value = radio.value;
+
+          if ((value === 'sine') || (value === 'square') || (value === 'sawtooth') || (value === 'triangle')) {
+            setType(value);
+          }
+        }
+      }
+    }, []);
+
+    return <OscillatorSelector {...args} type={type} onChange={onChange} />;
+  }
 };
 
 export const Primary = {
   ...Template,
   args: {
-    radioName  : 'oscillator-selector',
-    type       : 'sawtooth',
-    onChange   : (event: React.ChangeEvent<HTMLFormElement>) => {
-      alert(`${event.type} ${event.currentTarget.value}`);
-    }
+    radioName: 'oscillator-selector',
+    type     : 'sawtooth'
   }
 };
