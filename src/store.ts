34080d4bc9559c5ef5323f80178b13d63d0c71fd
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-03-04 22:56:27 +0900

    fix: Use `legacy_createStore` instead of `createStore`
    Should use Redux Toolkit instead of Redux.

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,11 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { legacy_createStore, compose, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
 import { rootReducer } from './reducers';
 
 const history    = createBrowserHistory();
 const middleware = routerMiddleware(history);
-const store      = createStore(rootReducer(), compose(applyMiddleware(middleware)));
+const store      = legacy_createStore(rootReducer(), compose(applyMiddleware(middleware)));
 
 export {
   store,
