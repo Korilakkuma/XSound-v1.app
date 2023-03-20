ac96a9cd439d9aa84cadfb7cf1701d07f9ef73b9
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-03-20 14:05:26 +0900

    feat: Add type for cloned `XSound` function

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,17 @@
 import type { store } from './store';
+import type {
+  OscillatorModule,
+  OneshotModule,
+  NoiseModule,
+  AudioModule,
+  MediaModule,
+  StreamModule,
+  ProcessorModule,
+  MixerModule,
+  MIDI,
+  MML,
+  Source
+} from 'xsound';
 
 export type RootState = ReturnType<typeof store.getState>;
 
@@ -25,4 +38,45 @@ export type MMLDescriptor = {
   description: string,
   melody: string,
   bass: string
+};
+
+// HACK:
+export function ClonedXSound(sourceName: 'oscillator'): OscillatorModule;
+export function ClonedXSound(sourceName: 'oneshot'): OneshotModule;
+export function ClonedXSound(sourceName: 'noise'): NoiseModule;
+export function ClonedXSound(sourceName: 'audio'): AudioModule;
+export function ClonedXSound(sourceName: 'media'): MediaModule;
+export function ClonedXSound(sourceName: 'stream'): StreamModule;
+export function ClonedXSound(sourceName: 'processor'): ProcessorModule;
+export function ClonedXSound(sourceName: 'mixer'): MixerModule;
+export function ClonedXSound(sourceName: 'midi'): MIDI;
+export function ClonedXSound(sourceName: 'mml'): MML;
+export function ClonedXSound(sourecName: string): Source | null {
+  switch (sourecName) {
+    case 'oscillator':
+      return {} as OscillatorModule;
+    case 'oneshot':
+      return {} as OneshotModule;
+    case 'noise':
+      return {} as NoiseModule;
+    case 'audio':
+      return {} as AudioModule;
+    case 'media':
+      return {} as MediaModule;
+    case 'stream':
+      return {} as StreamModule;
+    case 'processor':
+      return {} as ProcessorModule;
+    case 'mixer':
+      return {} as MixerModule;
+    case 'midi':
+      return {} as MIDI;
+    case 'mml':
+      return {} as MML;
+    default:
+      return null;
+  }
+}
+
+ClonedXSound.free = (_unusedSources: Source[]): void => {
 };
