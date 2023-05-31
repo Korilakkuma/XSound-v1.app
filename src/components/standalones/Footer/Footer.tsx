0f7510f93e0a0fd09fb8c91e3728b8c5a8db619a
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-05-31 17:02:03 +0900

    fix: Delete mixed content (Replace `http` to `https`)

--- src/components/standalones/Footer/Footer.tsx
+++ src/components/standalones/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="Footer">
       <p>Copyright (c) 2012 <a href="https://github.com/Korilakkuma" target="_blank" rel="noopener noreferrer">Tomohiro IKEDA</a></p>
-      <p><a href="http://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></p>
+      <p><a href="https://maoudamashii.jokersounds.com/" target="_blank" rel="noopener noreferrer" lang="ja">音楽素材/魔王魂</a></p>
     </footer>
   );
 };
