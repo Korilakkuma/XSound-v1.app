52dcc015e334e0426cce0805acc64ea81969e26a
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-08-07 16:28:08 +0900

    refactor: Use divided components

--- src/components/standalones/Analyser/Analyser.tsx
+++ src/components/standalones/Analyser/Analyser.tsx
@@ -1,14 +1,16 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { X } from 'xsound';
 
-import { formatAudioTime } from '/src/utils';
 import { Spacer } from '/src/components/atoms/Spacer';
 import { Switch } from '/src/components/atoms/Switch';
 import { ParameterController } from '/src/components/helpers/ParameterController';
+import { TimeOverviewer } from '/src/components/helpers/TimeOverviewer';
+import { TimeAnalyser } from '/src/components/helpers/TimeAnalyser';
+import { SpectrumAnalyser } from '/src/components/helpers/SpectrumAnalyser';
 
 import type { RootState } from '/src/types';
-import type { AnalyserParams, DragCallbackFunction, DragMode, FFTParams, TimeOverviewParams, TimeParams } from 'xsound';
+import type { AnalyserParams } from 'xsound';
 
 export type Props = {
   loadedApp: boolean;
@@ -17,22 +19,10 @@ export type Props = {
 export const Analyser: React.FC<Props> = (props: Props) => {
   const { loadedApp } = props;
 
-  const svgForTimeOverviewLeftRef = useRef<SVGSVGElement>(null);
-  const svgForTimeOverviewRightRef = useRef<SVGSVGElement>(null);
-  const svgForTimeDomainRef = useRef<SVGSVGElement>(null);
-  const svgForFrequencyDomainRef = useRef<SVGSVGElement>(null);
-
   const [loaded, setLoaded] = useState<boolean>(false);
   const [analyser, setAnalyser] = useState<boolean>(false);
-  const [dragTime, setDragTime] = useState<string>('00 : 00 - 00 : 00');
-  const [showDragTime, setShowDragTime] = useState<boolean>(false);
-  const [showTimeOverview, setShowTimeOverview] = useState<'left' | 'right'>('left');
 
   const active = useSelector((state: RootState) => state.analyserState);
-
-  const onClickChannelCallback = useCallback(() => {
-    setShowTimeOverview(showTimeOverview === 'right' ? 'left' : 'right');
-  }, [showTimeOverview]);
 
   const onChangeModeCallback = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const checked = event.currentTarget.checked;
@@ -114,16 +104,20 @@ export const Analyser: React.FC<Props> = (props: Props) => {
       return;
     }
 
-    if (
-      svgForTimeOverviewLeftRef.current === null ||
-      svgForTimeOverviewRightRef.current === null ||
-      svgForTimeDomainRef.current === null ||
-      svgForFrequencyDomainRef.current === null
-    ) {
-      return;
-    }
-
     if (loaded) {
+      const analyserParams: AnalyserParams = {
+        fftSize: 2048,
+        minDecibels: -60,
+        maxDecibels: 0,
+        smoothingTimeConstant: 0.8
+      };
+
+      X('mixer').module('analyser').param(analyserParams);
+      X('oneshot').module('analyser').param(analyserParams);
+      X('audio').module('analyser').param(analyserParams);
+      X('stream').module('analyser').param(analyserParams);
+      X('noise').module('analyser').param(analyserParams);
+
       X('mixer').module('analyser').domain('time').activate();
       X('mixer').module('analyser').domain('fft').activate();
       X('oneshot').module('analyser').domain('time').activate();
@@ -138,175 +132,15 @@ export const Analyser: React.FC<Props> = (props: Props) => {
       return;
     }
 
-    const analyserParams: AnalyserParams = {
-      fftSize: 2048,
-      minDecibels: -60,
-      maxDecibels: 0,
-      smoothingTimeConstant: 0.8
-    };
-
-    const timeParams: TimeParams = {
-      interval: -1,
-      type: 'uint',
-      styles: {
-        shape: 'line',
-        grid: 'rgba(255, 255, 255, 0.2)',
-        font: {
-          family: 'Arial',
-          size: '12px',
-          style: 'normal',
-          weight: 'normal'
-        },
-        width: 1.5,
-        right: 15
-      }
-    };
-
-    const fftParams: FFTParams = {
-      interval: -1,
-      type: 'float',
-      scale: 'logarithmic',
-      styles: {
-        shape: 'line',
-        grid: 'rgba(255, 255, 255, 0.2)',
-        font: {
-          family: 'Arial',
-          size: '12px',
-          style: 'normal',
-          weight: 'normal'
-        },
-        width: 1,
-        right: 15,
-        left: 40
-      }
-    };
-
-    const timeoverviewStyle: TimeOverviewParams = {
-      currentTime: {
-        color: 'rgba(255, 255, 255, 0.1)'
-      },
-      styles: {
-        shape: 'rect',
-        grid: 'rgba(255, 255, 255, 0.2)',
-        gradients: [
-          { offset: 0, color: 'rgba(0, 128, 255, 1.0)' },
-          { offset: 1, color: 'rgba(0,   0, 255, 1.0)' }
-        ],
-        font: {
-          family: 'Arial',
-          size: '12px',
-          style: 'normal',
-          weight: 'normal'
-        },
-        width: 0.5,
-        right: 15
-      }
-    };
-
-    X('mixer').module('analyser').param(analyserParams);
-    X('oneshot').module('analyser').param(analyserParams);
-    X('audio').module('analyser').param(analyserParams);
-    X('stream').module('analyser').param(analyserParams);
-    X('noise').module('analyser').param(analyserParams);
-
-    X('mixer').module('analyser').domain('time').setup(svgForTimeDomainRef.current).param(timeParams);
-    X('oneshot').module('analyser').domain('time').setup(svgForTimeDomainRef.current).param(timeParams);
-    X('audio').module('analyser').domain('time').setup(svgForTimeDomainRef.current).param(timeParams);
-    X('stream').module('analyser').domain('time').setup(svgForTimeDomainRef.current).param(timeParams);
-    X('noise').module('analyser').domain('time').setup(svgForTimeDomainRef.current).param(timeParams);
-
-    X('mixer').module('analyser').domain('fft').setup(svgForFrequencyDomainRef.current).param(fftParams);
-    X('oneshot').module('analyser').domain('fft').setup(svgForFrequencyDomainRef.current).param(fftParams);
-    X('audio').module('analyser').domain('fft').setup(svgForFrequencyDomainRef.current).param(fftParams);
-    X('stream').module('analyser').domain('fft').setup(svgForFrequencyDomainRef.current).param(fftParams);
-    X('noise').module('analyser').domain('fft').setup(svgForFrequencyDomainRef.current).param(fftParams);
-
-    const dragCallback: DragCallbackFunction = (event: MouseEvent | TouchEvent, startTime: number, endTime: number, mode: DragMode, direction: boolean) => {
-      if (event.type === 'mousedown' || event.type === 'touchstart') {
-        setShowDragTime(true);
-        return;
-      }
-
-      const time = direction ? endTime : startTime;
-
-      if (event.type === 'mousemove' || event.type === 'touchmove') {
-        const convertedStartTime = X.convertTime(startTime);
-        const convertedEndTime = X.convertTime(endTime);
-
-        setDragTime(`${formatAudioTime(convertedStartTime)} - ${formatAudioTime(convertedEndTime)}`);
-
-        if (time >= 0 && time <= X('audio').param('duration')) {
-          X('audio').param({ currentTime: time });
-        }
-
-        return;
-      }
-
-      if (event.type === 'mouseup' || event.type === 'touchend') {
-        switch (mode) {
-          case 'update':
-            if (time >= 0 && time <= X('audio').param('duration')) {
-              X('audio').module('analyser').domain('timeoverview', 0).update(time);
-              X('audio').module('analyser').domain('timeoverview', 1).update(time);
-              X('audio').param({ currentTime: time });
-            }
-
-            break;
-          case 'sprite':
-            X('audio').stop().start(startTime, endTime);
-
-            break;
-          default:
-            break;
-        }
-
-        setShowDragTime(false);
-      }
-    };
-
-    X('audio').module('analyser').domain('timeoverview', 0).setup(svgForTimeOverviewLeftRef.current).param(timeoverviewStyle).drag(dragCallback).activate();
-
-    X('audio').module('analyser').domain('timeoverview', 1).setup(svgForTimeOverviewRightRef.current).param(timeoverviewStyle).drag(dragCallback).activate();
-
     setLoaded(true);
   }, [loadedApp, loaded, active]);
 
   return (
     <div id="analyser-fieldset" aria-hidden={!active} className={`Analyser${active ? ' -active' : ''}`}>
-      <div className="Analyser__svg">
-        <dl>
-          <dt>
-            <label>
-              <button
-                type="button"
-                className={`Analyser__channelSelector -${showTimeOverview}`}
-                aria-label="Select Channel"
-                tabIndex={active ? 0 : -1}
-                onClick={onClickChannelCallback}
-              />
-              Time Overview [{showTimeOverview}]
-            </label>
-            {showDragTime ? <span className="Analyser__dragTime">{dragTime}</span> : null}
-          </dt>
-          <dd hidden={showTimeOverview === 'right'}>
-            <svg ref={svgForTimeOverviewLeftRef} width="1200" height="120" />
-          </dd>
-          <dd hidden={showTimeOverview === 'left'}>
-            <svg ref={svgForTimeOverviewRightRef} width="1200" height="120" />
-          </dd>
-        </dl>
-        <dl>
-          <dt>Time Domain</dt>
-          <dd>
-            <svg ref={svgForTimeDomainRef} width="420" height="120" />
-          </dd>
-        </dl>
-        <dl>
-          <dt>Frequency Domain</dt>
-          <dd>
-            <svg ref={svgForFrequencyDomainRef} width="420" height="120" />
-          </dd>
-        </dl>
+      <div className="Analyser__viewer">
+        <TimeOverviewer loadedApp={loadedApp} active={active} type="vector" />
+        <TimeAnalyser loadedApp={loadedApp} type="vector" />
+        <SpectrumAnalyser loadedApp={loadedApp} type="vector" />
       </div>
       <div className="Analyser__controllers">
         <Switch label="Audio Sprite" checked={analyser} labelAsText={true} tabIndex={active ? 0 : -1} onChange={onChangeModeCallback} />
