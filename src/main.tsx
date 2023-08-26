4254b95f6ec75b1025396c279d44f3f498fdf78e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-08-26 23:52:15 +0900

    refactor: Delete unnecessary type guard by using `!` operator

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,11 @@ import { Provider } from 'react-redux';
 import { App } from '/src/components/App';
 import { store } from '/src/store';
 
-const container = document.getElementById('app');
+const container = document.getElementById('app')!;
+const root = createRoot(container);
 
-if (container) {
-  const root = createRoot(container);
-
-  root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-}
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
